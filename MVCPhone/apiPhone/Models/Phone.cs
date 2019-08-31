6e2392080ebeeacd33d7c96940c471d09dd99c4d
andresvv99 <andres.viorels99@gmail.com>
2019-08-31 13:20:45 -0400

    Web api

--- MVCPhone/apiPhone/Models/Phone.cs
+++ MVCPhone/apiPhone/Models/Phone.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
-
-namespace MVCPhone.Models
+﻿namespace apiPhone.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public enum TypeContact
     {
         PhoneNumber,
@@ -13,7 +9,7 @@ namespace MVCPhone.Models
         Facebook,
         Twitter,
         LinkedIN
-    } 
+    }
 
     public class Phone
     {
